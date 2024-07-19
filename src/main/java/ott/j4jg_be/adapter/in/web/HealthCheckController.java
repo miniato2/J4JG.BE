f97546201c904ea9060af165b6cb0a7426bc4ffc
악덕 <rudejrqkr97@gmail.com>
2024-07-19 18:11:10 +0900

    feat: HealthCheckController 추가
    - 새로운 HealthCheckController를 추가

---
+++ src/main/java/ott/j4jg_be/adapter/in/web/HealthCheckController.java
@@ -0,0 +1,13 @@
+package ott.j4jg_be.adapter.in.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "OK";
+    }
+}
