41c7e5465c98fb0f1138e6daeafdde714c4c5ced
miniato2 <whgg456@gmail.com>
2024-07-24 12:59:45 +0900

    feat test api

--- src/main/java/ott/j4jg_be/adapter/in/web/API/TestAPIController.java
+++ src/main/java/ott/j4jg_be/adapter/in/web/API/TestAPIController.java
@@ -18,6 +18,7 @@ public class TestAPIController {
 
     private final RestTemplate restTemplate;
     private final ObjectMapper objectMapper;
+    private final JsonToDTOMapper jsonToDTOMapper;
 
     private String[] jobIds = {
             "10110", "873", "872", "669", "660", "900", "899", "1634",
@@ -43,29 +44,7 @@ public class TestAPIController {
                     JsonNode dataNode = rootNode.path("data");
                     if (dataNode.isArray()) {
                         for (JsonNode jobNode : dataNode) {
-                            JobinfoDTO jobDTO = new JobinfoDTO();
-                            jobDTO.setId(jobNode.path("id").asInt());
-
-                            JsonNode companyNode = jobNode.path("company");
-                            jobDTO.setCompanyId(companyNode.path("id").asInt());
-                            jobDTO.setCompanyName(companyNode.path("name").asText());
-                            jobDTO.setCompanyImg(jobNode.path("title_img").path("thumb").asText());
-                            jobDTO.setCompanyAvgRate(companyNode.path("application_response_stats").path("avg_rate").asText());
-                            jobDTO.setCompanyLvl(companyNode.path("application_response_stats").path("level").asText());
-
-                            JsonNode addressNode = jobNode.path("address");
-                            jobDTO.setLocation(addressNode.path("location").asText());
-                            jobDTO.setDistrict(addressNode.path("district").asText());
-
-                            jobDTO.setPosition(jobNode.path("position").asText());
-                            jobDTO.setCategoryCode(jobNode.path("category_tag").path("id").asInt());
-                            jobDTO.setAttractionTags(objectMapper.convertValue(jobNode.path("attraction_tags"), int[].class));
-                            jobDTO.setSkillTag(objectMapper.convertValue(jobNode.path("skill_tags"), int[].class));
-                            jobDTO.setAnnualFrom(jobNode.path("annual_from").asInt());
-                            jobDTO.setAnnualTo(jobNode.path("annual_to").asInt());
-                            jobDTO.setNewbie(jobNode.path("is_newbie").asBoolean());
-
-                            jobinfoDTOList.add(jobDTO);
+                            jobinfoDTOList.add(jsonToDTOMapper.mapper(jobNode));
                         }
                     }
                 } catch (Exception e) {
