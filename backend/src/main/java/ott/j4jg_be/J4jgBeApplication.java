3fa30a6b57b975900c1b24fda8013c9671abe7a0
악덕 <rudejrqkr97@gmail.com>
2024-07-31 21:03:21 +0900

    feat: .env 파일 로딩 설정 변경
    - .env 파일 로딩 설정 변경
    - Dotenv.configure().directory("../").load() 제거
    - dotenv.entries() 및 System.setProperty() 제거
    - J4jgBeApplication.java 파일 수정

--- backend/src/main/java/ott/j4jg_be/J4jgBeApplication.java
+++ backend/src/main/java/ott/j4jg_be/J4jgBeApplication.java
@@ -1,6 +1,5 @@
 package ott.j4jg_be;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,12 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class J4jgBeApplication {
 
     public static void main(String[] args) {
-        // root 디렉터리에서 .env 파일을 찾도록 설정
-        Dotenv dotenv = Dotenv.configure()
-                .directory("../")  // root로 설정
-                .load();
-        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
-
         SpringApplication.run(J4jgBeApplication.class, args);
     }
+
 }
