389ca00dd6cdf96d7be7ce3eb8ec7c11cd355014
miniato2 <whgg456@gmail.com>
2024-07-31 14:39:49 +0900

    feat : class 이름 및 패키지 구조 변경

---
+++ src/main/java/ott/j4jg_be/application/port/out/CallJobInfoAPIPort.java
@@ -0,0 +1,9 @@
+package ott.j4jg_be.application.port.out;
+
+import ott.j4jg_be.adapter.out.api.dto.JobInfoDTO;
+
+import java.util.List;
+
+public interface CallJobInfoAPIPort {
+    List<JobInfoDTO> callJobInfoAPI();
+}
