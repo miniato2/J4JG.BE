0317a3e5c79895d409339e4e6e7cf1ca32dabee7
seongmin <kkbl201612@gmail.com>
2024-08-05 13:26:21 +0900

    feat: SMS 전송 시 log기록 및 logstash 저장 추가

--- src/main/java/ott/j4jg_be/application/service/NotificationService.java
+++ src/main/java/ott/j4jg_be/application/service/NotificationService.java
@@ -1,6 +1,8 @@
 package ott.j4jg_be.application.service;
 
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import ott.j4jg_be.application.port.in.NotificationUsecase;
 import ott.j4jg_be.application.port.out.SnsPort;
@@ -9,11 +11,18 @@ import ott.j4jg_be.application.port.out.SnsPort;
 @RequiredArgsConstructor
 public class NotificationService implements NotificationUsecase {
 
-    private final SnsPort snsPort;
+    private static final Logger logger = LoggerFactory.getLogger(NotificationService.class);
 
+    private final SnsPort snsPort;
 
     @Override
     public void sendSMSNotification(String phoneNumber, String message) {
-        snsPort.publish(phoneNumber, message);
+        logger.info("Sending SMS to : {} / Content : {}", phoneNumber, message); 
+        try {
+            snsPort.publish(phoneNumber, message);
+            logger.info("SMS successfully sent to {}", phoneNumber);
+        } catch (Exception e) {
+            logger.error("Failed to send SMS to {}: {}", phoneNumber, e.getMessage());
+        }
     }
 }
