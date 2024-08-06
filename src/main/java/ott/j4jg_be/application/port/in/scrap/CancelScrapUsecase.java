fd9d9af5d0072c72ced64f8948143aeeaae902e9
seongmin <kkbl201612@gmail.com>
2024-08-06 16:07:37 +0900

    merge : base into develop_demo

---
+++ src/main/java/ott/j4jg_be/application/port/in/scrap/CancelScrapUsecase.java
@@ -0,0 +1,6 @@
+package ott.j4jg_be.application.port.in.scrap;
+
+public interface CancelScrapUsecase {
+
+    void cancelScrap(int scrapId);
+}
---
+++ src/main/java/ott/j4jg_be/application/service/user/UserService.java
@@ -0,0 +1,7 @@
+package ott.j4jg_be.application.service.user;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+}
