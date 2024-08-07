aaef826be9f595301d9aeecccbe247f2b365f002
chanyong <jcy168942@gmail.com>
2024-08-07 11:31:17 +0900

    refactor: 크롤링 news 부분 elasticsearch 에서 조회하는걸로 변경

--- src/main/java/ott/j4jg_be/application/service/collection/CrawlingNewsService.java
+++ src/main/java/ott/j4jg_be/application/service/collection/CrawlingNewsService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import ott.j4jg_be.adapter.out.crawler.dto.NewsDTO;
 import ott.j4jg_be.application.port.in.collection.CrawlingNewsUsecase;
 import ott.j4jg_be.application.port.out.collection.CrawlingNewsPort;
+import ott.j4jg_be.application.port.out.collection.JobInfoPort;
 
 import java.util.List;
 
@@ -17,18 +18,36 @@ import java.util.List;
 public class CrawlingNewsService implements CrawlingNewsUsecase {
 
     private final CrawlingNewsPort crawlingNewsPort;
+    private final JobInfoPort jobInfoPort;
     private final ObjectMapper objectMapper;
 
     @Override
     public void CrawlingNews() {
-        List<NewsDTO> newsList = crawlingNewsPort.crawlingNews();
+        int page = 0;
+        boolean hasMoreData;
 
-        newsList.forEach(news -> {
-            try {
-                log.info(objectMapper.writeValueAsString(news));
-            } catch (JsonProcessingException e) {
-                log.warn("error");
+        do {
+            List<String> companyNames = jobInfoPort.getCompanyNames(page, 100);
+            hasMoreData = !companyNames.isEmpty();
+
+            if (hasMoreData) {
+                processNews(companyNames);
+                page++;
             }
-        });
+        } while (hasMoreData);
+    }
+
+    private void processNews(List<String> companyNames) {
+        List<NewsDTO> newsList = crawlingNewsPort.crawlingNews(companyNames);
+
+        newsList.forEach(this::logNews);
+    }
+
+    private void logNews(NewsDTO news) {
+        try {
+            log.info(objectMapper.writeValueAsString(news));
+        } catch (JsonProcessingException e) {
+            log.warn("Error processing JSON", e);
+        }
     }
 }
