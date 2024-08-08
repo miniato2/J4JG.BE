31611198019981439a66d92d6ec6477973ab2848
악덕 <rudejrqkr97@gmail.com>
2024-08-09 07:05:07 +0900

    feat: #18 #19 소셜 미디어 인증 기능 추가
    - 소셜 미디어 클라이언트 등록 리포지토리 생성
    - Google 및 Kakao 클라이언트 등록 설정 추가
    - JWT 필터, 사용자 정의 핸들러 및 로그아웃 핸들러 구현
    - 보안 설정 및 권한 부여 작업 추가
    - 세션 관리를 STATELESS로 설정
    - CSRF, Form 로그인, HTTP Basic 인증 비활성화
    - OAuth2 로그인 및 사용자 정보 엔드포인트 설정 추가
    - 로그아웃 처리 및 클라이언트 등록 관련 메서드 추가

--- gateway/src/main/java/ott/j4jg_gateway/config/SecurityConfig.java
+++ gateway/src/main/java/ott/j4jg_gateway/config/SecurityConfig.java
@@ -1,55 +1,117 @@
 package ott.j4jg_gateway.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import ott.j4jg_gateway.jwt.JWTFilter;
+import ott.j4jg_gateway.oauth2.CustomLogoutSuccessHandler;
+import ott.j4jg_gateway.oauth2.CustomSuccessHandler;
 import ott.j4jg_gateway.service.CustomOAuth2UserService;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
 
     private final CustomOAuth2UserService customOAuth2UserService;
+    private final CustomLogoutSuccessHandler customLogoutSuccessHandler;
+    private final CustomSuccessHandler customSuccessHandler;
+    private final JWTFilter jwtFilter;
 
-    public SecurityConfig(CustomOAuth2UserService customOAuth2UserService) {
+    @Value("${spring.security.oauth2.client.registration.google.client-id}")
+    private String googleClientId;
+
+    @Value("${spring.security.oauth2.client.registration.google.client-secret}")
+    private String googleClientSecret;
+
+    @Value("${spring.security.oauth2.client.registration.google.redirect-uri}")
+    private String googleRedirectUri;
+
+    @Value("${spring.security.oauth2.client.registration.kakao.client-id}")
+    private String kakaoClientId;
+
+    @Value("${spring.security.oauth2.client.registration.kakao.client-secret}")
+    private String kakaoClientSecret;
+
+    @Value("${spring.security.oauth2.client.registration.kakao.redirect-uri}")
+    private String kakaoRedirectUri;
+
+    @Autowired
+    public SecurityConfig(CustomOAuth2UserService customOAuth2UserService,
+                          CustomLogoutSuccessHandler customLogoutSuccessHandler,
+                          CustomSuccessHandler customSuccessHandler,
+                          JWTFilter jwtFilter) {
         this.customOAuth2UserService = customOAuth2UserService;
+        this.customLogoutSuccessHandler = customLogoutSuccessHandler;
+        this.customSuccessHandler = customSuccessHandler;
+        this.jwtFilter = jwtFilter;
     }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        //csrf disable
-        http
-                .csrf((auth) -> auth.disable());
-
-        //From 로그인 방식 disable
-        http
-                .formLogin((auth) -> auth.disable());
-
-        //HTTP Basic 인증 방식 disable
-        http
-                .httpBasic((auth) -> auth.disable());
-
-        //oauth2
-        http
-                .oauth2Login((oauth2) -> oauth2
-                        .userInfoEndpoint((userInfoEndpointConfig -> userInfoEndpointConfig
-                                .userService(customOAuth2UserService))));
-
-        //경로별 인가 작업
-        http
-                .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/").permitAll()
-                        .anyRequest().authenticated());
-
-        //세션 설정 : STATELESS
-        http
-                .sessionManagement((session) -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.csrf(csrf -> csrf.disable())
+                .formLogin(formLogin -> formLogin.disable())
+                .httpBasic(httpBasic -> httpBasic.disable())
+                .oauth2Login(oauth2 -> oauth2
+                        .userInfoEndpoint(userInfoEndpoint -> userInfoEndpoint.userService(customOAuth2UserService))
+                        .successHandler(customSuccessHandler))
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().permitAll())
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessHandler(customLogoutSuccessHandler)
+                        .deleteCookies("JSESSIONID", "refreshToken")
+                        .invalidateHttpSession(true))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
+    }
+
+    @Bean
+    public ClientRegistrationRepository clientRegistrationRepository() {
+        return new InMemoryClientRegistrationRepository(
+                googleClientRegistration(),
+                kakaoClientRegistration()
+        );
+    }
+
+    private ClientRegistration googleClientRegistration() {
+        return ClientRegistration.withRegistrationId("google")
+                .clientId(googleClientId)
+                .clientSecret(googleClientSecret)
+                .scope("profile", "email")
+                .authorizationUri("https://accounts.google.com/o/oauth2/auth")
+                .tokenUri("https://oauth2.googleapis.com/token")
+                .userInfoUri("https://www.googleapis.com/oauth2/v3/userinfo")
+                .userNameAttributeName("sub")
+                .clientName("Google")
+                .redirectUri(googleRedirectUri)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .build();
+    }
+
+    private ClientRegistration kakaoClientRegistration() {
+        return ClientRegistration.withRegistrationId("kakao")
+                .clientId(kakaoClientId)
+                .clientSecret(kakaoClientSecret)
+                .scope("profile_nickname", "account_email", "phone_number")
+                .authorizationUri("https://kauth.kakao.com/oauth/authorize")
+                .tokenUri("https://kauth.kakao.com/oauth/token")
+                .userInfoUri("https://kapi.kakao.com/v2/user/me")
+                .userNameAttributeName("id")
+                .clientName("Kakao")
+                .redirectUri(kakaoRedirectUri)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .build();
     }
 }
