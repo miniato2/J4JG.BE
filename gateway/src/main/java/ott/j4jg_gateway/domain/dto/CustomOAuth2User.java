5b1ad57462e56e6876fd415ed1e4916ba446a00c
악덕 <rudejrqkr97@gmail.com>
2024-08-12 17:16:57 +0900

    feat: CustomOAuth2User 클래스 추가
    - 새로운 CustomOAuth2User 클래스를 추가
    - 이 클래스는 DefaultOAuth2User를 확장하여 사용자 정보를 저장
    - 사용자의 공급자, 공급자 ID, 이메일, 전화번호를 저장
    - 사용자 정보를 검증하는 validateAttributes 메서드를 추가
    - 사용자 정보를 반환하는 get 메서드를 구현

---
+++ gateway/src/main/java/ott/j4jg_gateway/domain/dto/CustomOAuth2User.java
@@ -0,0 +1,79 @@
+package ott.j4jg_gateway.domain.dto;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+
+import java.util.Collection;
+import java.util.Map;
+
+public class CustomOAuth2User extends DefaultOAuth2User {
+    private static final Logger logger = LoggerFactory.getLogger(CustomOAuth2User.class);
+
+    private String provider;
+    private String providerId;
+    private String email;
+    private String phoneNumber;
+
+    public CustomOAuth2User(
+            Collection<? extends GrantedAuthority> authorities,
+            Map<String, Object> attributes,
+            String provider,
+            String providerId,
+            String email,
+            String phoneNumber,
+            String userIdAttributeName) {
+        super(authorities, attributes, userIdAttributeName);
+        this.provider = provider;
+        this.providerId = providerId;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+
+        logger.info("CustomOAuth2User created with provider: {}, providerId: {}, email: {}, phoneNumber: {}", provider, providerId, email, phoneNumber);
+
+        validateAttributes();
+    }
+
+    private void validateAttributes() {
+        if (this.provider == null) {
+            logger.error("Provider is null");
+        }
+        if (this.providerId == null) {
+            logger.error("ProviderId is null");
+        }
+        if (this.email == null) {
+            logger.error("Email is null");
+        }
+        if (this.phoneNumber == null) {
+            logger.warn("PhoneNumber is null");
+            this.phoneNumber = "N/A"; // Set default value if needed
+        }
+    }
+
+    public String getProvider() {
+        return provider;
+    }
+
+    public String getProviderId() {
+        return providerId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "CustomOAuth2User{" +
+                "provider='" + provider + '\'' +
+                ", providerId='" + providerId + '\'' +
+                ", email='" + email + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                '}';
+    }
+}
