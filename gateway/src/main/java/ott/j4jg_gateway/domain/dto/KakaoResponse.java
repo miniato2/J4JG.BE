5243a1fba4507745260e31460e70a784a420a011
악덕 <rudejrqkr97@gmail.com>
2024-08-12 17:18:30 +0900

    feat: #18 #19 카카오 및 구글 OAuth2 응답 DTO 추가
    - 카카오 및 구글 OAuth2 응답 DTO를 추가
    - KakaoResponse 및 GoogleResponse 클래스를 작성
    - KakaoResponse는 Kakao 사용자 속성을 포함
    - GoogleResponse는 Google 사용자 속성을 포함
    - KakaoResponse에는 사용자 이메일 및 전화번호를 반환하는 메서드가 추가
    - GoogleResponse에는 사용자 이메일을 반환하는 메서드가 추가
    - Google API는 사용자 휴대전화번호를 기본적으로 제공하지 않음

---
+++ gateway/src/main/java/ott/j4jg_gateway/domain/dto/KakaoResponse.java
@@ -0,0 +1,37 @@
+package ott.j4jg_gateway.domain.dto;
+
+import java.util.Map;
+import java.util.Optional;
+
+public class KakaoResponse implements OAuth2Response {
+
+    private final Map<String, Object> attributes; // Kakao 사용자 속성
+
+    public KakaoResponse(Map<String, Object> attributes) {
+        this.attributes = attributes;
+    }
+
+    @Override
+    public String getProvider() {
+        return "kakao"; // 제공자 이름 반환
+    }
+
+    @Override
+    public String getProviderId() {
+        return attributes.get("id").toString();
+
+    }
+
+    @Override
+    public String getEmail() {
+        Map<String, Object> kakaoAccount = (Map<String, Object>) attributes.get("kakao_account");
+        return kakaoAccount != null ? (String) kakaoAccount.get("email") : null;
+    }
+
+    @Override
+    public String getPhoneNumber() {
+        Map<String, Object> kakaoAccount = (Map<String, Object>) attributes.get("kakao_account");
+        return kakaoAccount != null ? (String) kakaoAccount.get("phone_number") : null;
+    }
+
+}
