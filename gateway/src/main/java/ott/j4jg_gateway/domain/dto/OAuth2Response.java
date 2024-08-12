94169709eb0dc6a7c35c91b066e850163418df79
악덕 <rudejrqkr97@gmail.com>
2024-08-12 17:16:07 +0900

    feat: OAuth2Response 인터페이스에 PROVIDER 타입을 String으로 변경
    - OAuth2Response 인터페이스에서 PROVIDER 타입을 String으로 변경합니다.

--- gateway/src/main/java/ott/j4jg_gateway/domain/dto/OAuth2Response.java
+++ gateway/src/main/java/ott/j4jg_gateway/domain/dto/OAuth2Response.java
@@ -1,11 +1,10 @@
 package ott.j4jg_gateway.domain.dto;
 
-import ott.j4jg_gateway.domain.enums.PROVIDER;
 
 public interface OAuth2Response {
 
     // 제공자 (kakao, google 등)
-    PROVIDER getProvider();
+    String getProvider();
 
     // 제공자에서 발급해주는 아이디(번호)
     String getProviderId();
