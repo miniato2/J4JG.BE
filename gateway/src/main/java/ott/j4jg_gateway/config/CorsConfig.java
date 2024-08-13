944246de431ad41a0a9e08f9daffee6c7f9c454f
악덕 <rudejrqkr97@gmail.com>
2024-08-13 09:19:43 +0900

    feat: CORS 구성 추가
    - CORS 구성을 추가하여 모든 출처를 허용
    - 자격 증명을 사용하지 않도록 설정
    - 허용된 헤더 및 메서드를 구성
    - 노출된 헤더를 설정하여 보안을 강화

---
+++ gateway/src/main/java/ott/j4jg_gateway/config/CorsConfig.java
@@ -0,0 +1,26 @@
+package ott.j4jg_gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(false); // 자격 증명 사용 안 함
+        config.setAllowedOrigins(Arrays.asList("*")); // 모든 출처 허용
+        config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization"));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setExposedHeaders(Arrays.asList("Authorization", "Set-Cookie", "Cookie"));
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
