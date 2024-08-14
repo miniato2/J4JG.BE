e312a9c61348d5f286c229957566c032f23c4a15
악덕 <rudejrqkr97@gmail.com>
2024-08-14 14:50:59 +0900

    feat: JWTUtil에 새로운 기능 추가
    - 'getProviderId' 메서드를 추가하여 제공자 ID를 반환
    - 'createJwt' 메서드에서 providerId의 데이터 타입을 String으로 변경

--- gateway/src/main/java/ott/j4jg_gateway/jwt/JWTUtil.java
+++ gateway/src/main/java/ott/j4jg_gateway/jwt/JWTUtil.java
@@ -33,7 +33,6 @@ public class JWTUtil {
         this.refreshTokenExpirationTime = refreshTokenExpirationTime;
     }
 
-    // 토큰에서 모든 클레임을 추출하는 메서드
     public Claims getAllClaimsFromToken(String token) {
         try {
             return Jwts.parserBuilder()
@@ -41,28 +40,16 @@ public class JWTUtil {
                     .build()
                     .parseClaimsJws(token)
                     .getBody();
-        } catch (ExpiredJwtException e) {
-            logger.warning("토큰이 만료되었습니다: " + e.getMessage());
-            throw e;
-        } catch (UnsupportedJwtException e) {
-            logger.warning("지원되지 않는 JWT입니다: " + e.getMessage());
-            throw e;
-        } catch (MalformedJwtException e) {
-            logger.warning("잘못된 JWT 형식입니다: " + e.getMessage());
-            throw e;
-        } catch (SignatureException e) {
-            logger.warning("유효하지 않은 서명입니다: " + e.getMessage());
-            throw e;
-        } catch (IllegalArgumentException e) {
-            logger.warning("잘못된 클레임입니다: " + e.getMessage());
-            throw e;
         } catch (JwtException e) {
             logger.warning("JWT 예외 발생: " + e.getMessage());
             throw e;
         }
     }
 
-    // 토큰에서 클레임을 파싱하는 메서드
+    public String getProviderId(String token) {
+        return getAllClaimsFromToken(token).get("provider_id", String.class);
+    }
+
     public Jws<Claims> parseClaims(String token) {
         try {
             return Jwts.parserBuilder()
@@ -75,34 +62,28 @@ public class JWTUtil {
         }
     }
 
-    // 권한을 리스트로 반환하는 메서드
     public List<GrantedAuthority> getAuthorities(String role) {
         GrantedAuthority authority = new SimpleGrantedAuthority(role);
         return Collections.singletonList(authority);
     }
 
-    // 토큰에서 이메일을 추출하는 메서드
     public String getEmail(String token) {
         return getAllClaimsFromToken(token).get("email", String.class);
     }
 
-    // 토큰에서 제공자를 추출하는 메서드
     public String getProvider(String token) {
         return getAllClaimsFromToken(token).get("provider", String.class);
     }
 
-    // 토큰에서 역할을 추출하는 메서드
     public String getRole(String token) {
         return getAllClaimsFromToken(token).get("role", String.class);
     }
 
-    // 토큰의 만료 여부를 확인하는 메서드
     public boolean isExpired(String token) {
         return getAllClaimsFromToken(token).getExpiration().before(new Date());
     }
 
-    // JWT를 생성하는 메서드
-    public String createJwt(String category, String email, String role, String provider, String phoneNumber, String providerId, long expiredMs) { // providerId를 String으로 변경
+    public String createJwt(String category, String email, String role, String provider, String phoneNumber, String providerId, long expiredMs) {
         return Jwts.builder()
                 .claim("category", category)
                 .claim("email", email)
@@ -116,26 +97,10 @@ public class JWTUtil {
                 .compact();
     }
 
-    // 토큰의 유효성을 검사하는 메서드
     public boolean validateToken(String token) {
         try {
             Jws<Claims> claims = parseClaims(token);
             return !claims.getBody().getExpiration().before(new Date());
-        } catch (ExpiredJwtException e) {
-            logger.warning("토큰이 만료되었습니다: " + e.getMessage());
-            return false;
-        } catch (UnsupportedJwtException e) {
-            logger.warning("지원되지 않는 JWT입니다: " + e.getMessage());
-            return false;
-        } catch (MalformedJwtException e) {
-            logger.warning("잘못된 JWT 형식입니다: " + e.getMessage());
-            return false;
-        } catch (SignatureException e) {
-            logger.warning("유효하지 않은 서명입니다: " + e.getMessage());
-            return false;
-        } catch (IllegalArgumentException e) {
-            logger.warning("잘못된 클레임입니다: " + e.getMessage());
-            return false;
         } catch (JwtException e) {
             logger.warning("JWT 예외 발생: " + e.getMessage());
             return false;
