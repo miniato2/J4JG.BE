9e603080fe3396b978a606eae2097c1dd49a6033
seongmin <kkbl201612@gmail.com>
2024-08-14 13:31:53 +0900

    no message

--- gateway/src/main/java/ott/j4jg_gateway/config/GatewayConfiguration.java
+++ gateway/src/main/java/ott/j4jg_gateway/config/GatewayConfiguration.java
@@ -1,5 +1,6 @@
 package ott.j4jg_gateway.config;
 
+
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
