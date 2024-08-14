bc5fa65c48b58b490fad88143e91b505e9a2e7a5
악덕 <rudejrqkr97@gmail.com>
2024-08-14 14:37:11 +0900

    feat: #25 RefreshToken 엔티티 수정

--- gateway/src/main/java/ott/j4jg_gateway/domain/entity/RefreshToken.java
+++ gateway/src/main/java/ott/j4jg_gateway/domain/entity/RefreshToken.java
@@ -1,40 +1,31 @@
 package ott.j4jg_gateway.domain.entity;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.springframework.data.redis.core.RedisHash;
+import lombok.*;
 import org.springframework.data.annotation.Id;
-
+import org.springframework.data.annotation.Transient;
+import org.springframework.data.redis.core.RedisHash;
 import java.io.Serializable;
 
-@RedisHash("RefreshToken")
-@JsonInclude(JsonInclude.Include.NON_NULL)
-@Data
+@Getter
+@Setter
 @Builder
-@NoArgsConstructor
 @AllArgsConstructor
+@NoArgsConstructor
+@RedisHash("RefreshToken")
+
 public class RefreshToken implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
     @Id
-    @JsonProperty("providerId")  // JSON에서 'providerId'로 직렬화/역직렬화
     private String providerId; // Redis의 키 역할을 하는 필드
 
-    @JsonProperty("token")
     private String token; // 실제 토큰 값
 
-    @JsonProperty("userEmail")
     private String userEmail; // 사용자 이메일
 
-    @JsonProperty("provider")
     private String provider; // 제공자 정보
 
-    @JsonProperty("expiration")
     private long expiration; // 만료 시간
 
 }
